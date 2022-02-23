b47212d7edf845b49c9554b8648ddc39b9aab7a3
Scott Green <sagreenxyz@gmail.com>
2022-02-22 22:04:42 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+1. git init
+2. git branch -m master main
+3. npm init -y
+4. echo "node_modules\n" > .gitignore
+5. npm install express
+6. code .
+*/
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000);
+
+/*
+7. node index.js (or nodemon)
+8. localhost:3000
+*/
